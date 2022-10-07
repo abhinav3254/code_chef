4e2cd71d3c15c974d3f4109eba7d8c6798ed3ffb
abhinav3254 <abhinav3254@gmail.com>
2022-10-07 20:27:18 +0530

    changes

---
+++ october-long-challenge/CodeChef1.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef1 {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+
+        for (int i = 0; i < testCase; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
