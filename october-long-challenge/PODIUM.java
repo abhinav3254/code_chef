ad13b877075f2e50842551da54cc087237b61bb3
abhinav3254 <abhinav3254@gmail.com>
2022-10-07 20:35:22 +0530

    PODIUM q1 solved code-chef oct long challenge

---
+++ october-long-challenge/PODIUM.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PODIUM {
+    static class Codechef1
+    {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            // your code goes here
+
+            Scanner sc = new Scanner(System.in);
+
+            int testCase = sc.nextInt();
+
+            for (int i = 0; i < testCase; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                System.out.println(a+b);
+            }
+        }
+    }
+}
