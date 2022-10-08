232d9e78e4b3b08c3dcb5b5084ce57a523aee26d
abhinav3254 <abhinav3254@gmail.com>
2022-10-08 09:53:50 +0530

    SINGLEOP1 error

--- october-long-challenge/SINGLEOP1.java
+++ october-long-challenge/SINGLEOP1.java
@@ -11,8 +11,8 @@ public class SINGLEOP1 {
 
         for (int j = 0;j<t;j++) {
             String n = br.readLine();
-            String s = br.readLine();
-            int x = Integer.parseInt(s,2);
+            int s = Integer.parseInt(br.readLine());
+            int x = binaryToDecimal(s);
             int []arr = new int[x+1];
             for (int i = 0; i <= x; i++) {
                 arr[i] = x^(x/(pow(i,2)));
@@ -46,6 +46,38 @@ public class SINGLEOP1 {
         }
         return ans;
     }
+
+    static int binaryToDecimal(int n)
+    {
+        int num = n;
+        int dec_value = 0;
+
+        // Initializing base
+        // value to 1, i.e 2^0
+        int base = 1;
+
+        int temp = num;
+        while (temp > 0) {
+            int last_digit = temp % 10;
+            temp = temp / 10;
+
+            dec_value += last_digit * base;
+
+            base = base * 2;
+        }
+
+        return dec_value;
+    }
+
+    /*
+    *
+    *
+    * Fails on this test case
+    3
+    10101
+    5
+    1011011
+    * */
 
     /*
     *
