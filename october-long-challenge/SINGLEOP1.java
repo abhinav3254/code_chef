cea7da1f6171952f6f251a4ad1fd617f09d0a27e
abhinav3254 <abhinav3254@gmail.com>
2022-10-08 09:36:14 +0530

    SINGLEOP1 error

--- october-long-challenge/SINGLEOP1.java
+++ october-long-challenge/SINGLEOP1.java
@@ -5,7 +5,47 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SINGLEOP1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
+        for (int j = 0;j<t;j++) {
+            String n = br.readLine();
+            String s = br.readLine();
+            int x = Integer.parseInt(s,2);
+            int []arr = new int[x+1];
+            for (int i = 0; i <= x; i++) {
+                arr[i] = x^(x/(pow(i,2)));
+            }
+//        System.out.println(Arrays.toString(arr));
+            int index = Integer.MIN_VALUE;
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == max) {
+
+                } else if (max<arr[i]) {
+                    max = arr[i];
+                    index = i;
+                }
+            }
+            System.out.println(index);
+        }
+    }
+
+
+    static public int pow(int power , int base) {
+        int ans = 1;
+        while (power > 0){
+            if ((power&1)==1){
+                // if == 0 there is no point to calculate ans
+                ans = ans * base;
+            }
+            base = base * base;
+            // keep dividing power by 2 using right shift
+            power = power >> 1;
+        }
+        return ans;
+    }
 
     /*
     *
